8162b0b91e0bc524dab4a0da5ec733518eb12983
TatsuyaUchida0326 <sopratico0326@gmail.com>
2025-06-25 06:27:59 +0900

    Finish todo_list_cru(ReactでTodoアプリを作ろう_2完了)

--- src/components/todos/index.tsx
+++ src/components/todos/index.tsx
@@ -3,40 +3,89 @@ import React, { useState } from 'react';
 // "Todo" 型の定義をコンポーネント外で行います
 type Todo = {
   title: string; // プロパティ content は文字列型
+  readonly id: number;
 };
+
 
 // Todo コンポーネントの定義
 const Todo: React.FC = () => {
-  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]); // Todo配列として初期化
+    const [text, setText] = useState(''); // テキスト入力用
+    const [nextId, setNextId] = useState(1); // 次のTodoのIDを保持するステート
 
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // タスクの追加処理をここに追加します
-    console.log('Todo added:', todo);
-    setTodo(''); // フォームをリセット
-  };
 
+    // todos ステートを更新する関数
+    const handleSubmit = () => {
+      // 何も入力されていなかったらリターン
+      if (!text) return;
+
+
+      // 新しい Todo を作成
+      const newTodo: Todo = {
+        title: text, // text ステートの値を content プロパティへ
+        id: nextId,
+      };
+
+      setTodos((prevTodos) => [newTodo, ...prevTodos]);
+      setNextId(nextId + 1); // 次の ID を更新
+
+      // フォームへの入力をクリアする
+      setText('');
+    };
+
+    const handleEdit = (id: number, value: string) => {
+      setTodos((todos) => {
+        const newTodos = todos.map((todo) => {
+          if (todo.id === id) {
+            // 新しいオブジェクトを作成して返す
+            return { ...todo, title: value };
+          }
+          return todo;
+        });
+
+
+        // todos ステートが書き換えられていないかチェック
+        console.log('=== Original todos ===');
+        todos.map((todo) => {
+          console.log(`id: ${todo.id}, title: ${todo.title}`);
+        });
+
+
+        return newTodos;
+      });
+    };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault(); // フォームのデフォルト動作を防ぐ
+          handleSubmit(); // handleSubmit 関数を呼び出す
+        }}
+      >
         <input
           type="text"
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
+          value={text} // フォームの入力値をステートにバインド
+          onChange={(e) => setText(e.target.value)} // 入力値が変わった時にステートを更新
         />
-        <input
-          type="submit"
-          value="追加"
-        />
+         <button className="insert-btn" type="submit">追加</button>{/* ボタンをクリックしてもonSubmitをトリガーしない */}
       </form>
-      {/* ↓ DOM のリアクティブな反応を見るためのサンプル */}
-      <p>{todo}</p>
-      {/* ↑ あとで削除 */}
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="text"
+                value={todo.title}
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
-
 
 export default Todo;
